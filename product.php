e361ca7650f76d4808ca434463d017226daec9f8
ShawnKohEnWee <1211211185@student.mmu.edu.my>
2024-04-18 14:07:06 +0800

    change table

--- product.php
+++ product.php
@@ -60,6 +60,7 @@
 <div id="container">
   <h1>F1 Product Shop</h1>
     <div class="product-list">
+      <h2>Clothes</h2>
             <table class="product-table" border="1" width="700px" height="100px">
                 <tr>
                     <th>Product Code</th>
@@ -68,7 +69,7 @@
                     <th>Product size</th>
                     <th>Product Description</th>
                     <th>product Price</th>			
-                    <th colspan="3">Action</th>
+                    <th>Action</th>
                 </tr>
     
                 <?php
@@ -86,6 +87,41 @@
                         <td><?php echo $row["product_size"];?></td>
                         <td><?php echo $row["description"];?></td>
                         <td>£<?php echo $row["product_price"];?></td>
+                        <td><a href="Order.php">Order Now</a></td>
+                        <!--<td><a href="manage product.php?edit&procode=<?php echo $row['product_code']; ?>">Edit</a></td>-->
+                        <!--<td><a href="product_list.php?del&procode=<?php echo $row['product_code']; ?>" onclick="return confirmation();">Delete</a></td>-->
+                    </tr>
+                    <?php
+                    
+                    }		
+                
+                ?>
+    
+                
+            </table>
+            <h2>Tickets</h2>
+            <table class="product-table" border="1" width="700px" height="100px">
+                <tr>
+                    <th>Ticket ID</th>
+                    <th>Race ID</th>
+                    <th>Stand</th>
+                    <th>Ticket Price</th>			
+                    <th>Action</th>
+                </tr>
+    
+                <?php
+                
+                $result = mysqli_query($connect, "select * from ticket");	
+                while($row = mysqli_fetch_assoc($result))
+                    {
+                        
+                    ?>			
+    
+                    <tr>
+                        <td><?php echo $row["ticketID"];?></td>
+                        <td><?php echo $row["raceID"];?></td>
+                        <td><?php echo $row["stand"];?></td>
+                        <td>£<?php echo $row["ticket_price"];?></td>
                         <td><a href="Order.php">Order Now</a></td>
                         <!--<td><a href="manage product.php?edit&procode=<?php echo $row['product_code']; ?>">Edit</a></td>-->
                         <!--<td><a href="product_list.php?del&procode=<?php echo $row['product_code']; ?>" onclick="return confirmation();">Delete</a></td>-->
