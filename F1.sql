eebb8fad6cdbba7026e505780496c869080b0234
ShawnKohEnWee <1211211185@student.mmu.edu.my>
2024-04-26 03:33:52 +0800

    change database

--- F1.sql
+++ F1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2024 at 02:41 PM
+-- Generation Time: Apr 25, 2024 at 09:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -51,7 +51,8 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`feedback_no.`, `feedback`) VALUES
-(1, 'good');
+(1, 'Nice product'),
+(2, 'Good service bro!'),
 
 -- --------------------------------------------------------
 
@@ -61,7 +62,7 @@ INSERT INTO `feedback` (`feedback_no.`, `feedback`) VALUES
 
 CREATE TABLE `order` (
   `OrderID` int(11) NOT NULL,
-  `UserID` int(5) DEFAULT NULL,
+  `id` int(5) DEFAULT NULL,
   `payment_method` varchar(50) NOT NULL,
   `transactionID` int(11) DEFAULT NULL,
   `payment_status` varchar(20) NOT NULL
@@ -74,13 +75,20 @@ CREATE TABLE `order` (
 --
 
 CREATE TABLE `product` (
-  `product_code` int(4) NOT NULL,
+  `product_code` int(8) NOT NULL,
   `product_img` text NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `product_size` varchar(5) NOT NULL,
   `description` varchar(500) NOT NULL,
   `product_price` decimal(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_code`, `product_img`, `product_name`, `product_size`, `description`, `product_price`) VALUES
+(13334182, 'images/product/Red Bull clothes(1).jpg', 'Oracle Red Bull Racing 2023 Team Polo', '55-80', '<ul>\r\n              <li>84% polyester/16% Elastane</li>\r\n              <li>Short sleeves</li>\r\n              <li>Quarter zip neck</li>\r\n              <li>Polo collar</li>\r\n              <li>Regular fit</li>\r\n              <li>Heat-sealed team and sponsor graphics</li>\r\n              <li>Officially licensed</li>\r\n            </ul>', 48.75);
 
 -- --------------------------------------------------------
 
@@ -90,7 +98,7 @@ CREATE TABLE `product` (
 
 CREATE TABLE `ticket` (
   `ticketID` int(4) NOT NULL,
-  `raceID` int(4) NOT NULL,
+  `race` varchar(1000) NOT NULL,
   `stand` varchar(100) NOT NULL,
   `ticket_price` decimal(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -118,9 +126,9 @@ CREATE TABLE `transaction` (
 
 CREATE TABLE `user` (
   `id` int(5) NOT NULL,
-  `username` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `username` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -128,7 +136,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
-(11, 'how', 'howchongxian@gmail.com', '123');
+(1, 'shawnkew0828', 'shawnkohenwee@gmail.com', 'Mt4-8+28'),
+(2, 'User', 'user@hotmail.com', 'Mt021-036'),
+(3, 'Shawnkew', 'shawnkew@hotmail.com', 'Mt4-8+28'),
+(4, 'test', 'test@hotmail.com', 't20e5s19t20');
 
 --
 -- Indexes for dumped tables
@@ -151,7 +162,7 @@ ALTER TABLE `feedback`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`OrderID`),
-  ADD KEY `UserID` (`UserID`);
+  ADD KEY `id` (`id`) USING BTREE;
 
 --
 -- Indexes for table `ticket`
@@ -170,7 +181,7 @@ ALTER TABLE `transaction`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`);
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -181,13 +192,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `contact_code` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `contact_code` int(3) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feedback_no.` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `feedback_no.` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order`
@@ -211,7 +222,7 @@ ALTER TABLE `transaction`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -221,7 +232,7 @@ ALTER TABLE `user`
 -- Constraints for table `order`
 --
 ALTER TABLE `order`
-  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `transaction`
