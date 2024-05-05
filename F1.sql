6d0905cc88c4a238e8ca4696b6c43b45deee46c0
1211110578 <1211110578@student.mmu.edu.my>
2024-05-05 18:10:39 +0800

    upload database, other code dont check first

--- F1.sql
+++ F1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2024 at 12:52 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: May 05, 2024 at 12:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -52,7 +52,8 @@ CREATE TABLE `feedback` (
 
 INSERT INTO `feedback` (`feedback_no.`, `feedback`) VALUES
 (1, 'Nice product'),
-(2, 'Good service bro!');
+(2, 'Good service bro!'),
+(3, 'Nice website');
 
 -- --------------------------------------------------------
 
@@ -88,52 +89,7 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_code`, `product_img`, `product_name`, `product_size`, `description`, `product_price`) VALUES
-(13334182, 'images/product/Red Bull clothes(1).jpg', 'Oracle Red Bull Racing 2023 Team Polo', '55-80', '<ul>\r\n              <li>84% polyester/16% Elastane</li>\r\n              <li>Short sleeves</li>\r\n              <li>Quarter zip neck</li>\r\n              <li>Polo collar</li>\r\n              <li>Regular fit</li>\r\n              <li>Heat-sealed team and sponsor graphics</li>\r\n              <li>Officially licensed</li>\r\n            </ul>', 48.75);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `shopping_cart`
---
-
-CREATE TABLE `shopping_cart` (
-  `cart_id` int(11) NOT NULL,
-  `id` int(11) DEFAULT NULL,
-  `product_code` int(8) DEFAULT NULL,
-  `product_name` varchar(100) DEFAULT NULL,
-  `product_img` text NOT NULL,
-  `quantity` int(11) DEFAULT 1,
-  `product_price` decimal(7,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `shopping_cart`
---
-
-INSERT INTO `shopping_cart` (`cart_id`, `id`, `product_code`, `product_name`, `product_img`, `quantity`, `product_price`) VALUES
-(5, 3, 13334182, 'Oracle Red Bull Racing 2023 Team Polo                          ', 'images/product/Red Bull clothes(1).jpg                          ', 2, 48.75);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `shopping_cart2`
---
-
-CREATE TABLE `shopping_cart2` (
-  `cart_id` int(11) NOT NULL,
-  `id` int(11) DEFAULT NULL,
-  `ticketID` int(4) DEFAULT NULL,
-  `race` varchar(1000) DEFAULT NULL,
-  `quantity` int(11) DEFAULT 1,
-  `ticket_price` decimal(8,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `shopping_cart2`
---
-
-INSERT INTO `shopping_cart2` (`cart_id`, `id`, `ticketID`, `race`, `quantity`, `ticket_price`) VALUES
-(3, 3, 1, 'FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2024                          ', 1, 10000.00);
+(13334182, 'images/product/Red Bull clothes(1).jpg', 'Oracle Red Bull Racing 2023 Team Polo', '55-80', '<ul>\r\n              <li>84% polyester/16% Elastane</li>\r\n              <li>Short sleeves</li>\r\n              <li>Quarter zip neck</li>\r\n              <li>Polo collar</li>\r\n              <li>Regular fit</li>\r\n              <li>Heat-sealed team and sponsor graphics</li>\r\n              <li>Officially licensed</li>\r\n            </ul>', '48.75');
 
 -- --------------------------------------------------------
 
@@ -145,16 +101,8 @@ CREATE TABLE `ticket` (
   `ticketID` int(4) NOT NULL,
   `race` varchar(1000) NOT NULL,
   `stand` varchar(100) NOT NULL,
-  `ticket_price` decimal(8,2) NOT NULL
+  `ticket_price` decimal(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `ticket`
---
-
-INSERT INTO `ticket` (`ticketID`, `race`, `stand`, `ticket_price`) VALUES
-(1, 'FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2024', 'HOSPITALITY - FORMULA ONE PADDOCK CLUB', 10000.00),
-(2, 'FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2024', 'Marina Central Grandstand', 850.00);
 
 -- --------------------------------------------------------
 
@@ -181,19 +129,19 @@ CREATE TABLE `user` (
   `id` int(5) NOT NULL,
   `username` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `password` varchar(20) NOT NULL,
+  `role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
-(1, 'shawnkew0828', 'shawnkohenwee@gmail.com', 'Mt4-8+28'),
-(2, 'User', 'user@hotmail.com', 'Mt021-036'),
-(3, 'Shawnkew', 'shawnkew@hotmail.com', 'Mt4-8+28'),
-(4, 'test', 'test@hotmail.com', 't20e5s19t20'),
-(5, 'howchongxian', 'howchongxian@gmail.com', '1234');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`) VALUES
+(1, 'superadmin', 'superadmin@gmail.com', 'Mt4-8+28', 'superadmin'),
+(2, 'admin', 'admin@hotmail.com', 'Mt021-036', 'admin'),
+(3, 'user', 'user@hotmail.com', 'Mt4-8+28', 'user'),
+(4, 'test', 'test@hotmail.com', 't20e5s19t20', 'user');
 
 --
 -- Indexes for dumped tables
@@ -217,18 +165,6 @@ ALTER TABLE `feedback`
 ALTER TABLE `order`
   ADD PRIMARY KEY (`OrderID`),
   ADD KEY `id` (`id`) USING BTREE;
-
---
--- Indexes for table `shopping_cart`
---
-ALTER TABLE `shopping_cart`
-  ADD PRIMARY KEY (`cart_id`);
-
---
--- Indexes for table `shopping_cart2`
---
-ALTER TABLE `shopping_cart2`
-  ADD PRIMARY KEY (`cart_id`);
 
 --
 -- Indexes for table `ticket`
@@ -264,7 +200,7 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feedback_no.` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `feedback_no.` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `order`
@@ -273,22 +209,10 @@ ALTER TABLE `order`
   MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `shopping_cart`
---
-ALTER TABLE `shopping_cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `shopping_cart2`
---
-ALTER TABLE `shopping_cart2`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT for table `ticket`
 --
 ALTER TABLE `ticket`
-  MODIFY `ticketID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ticketID` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `transaction`
@@ -300,7 +224,7 @@ ALTER TABLE `transaction`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
---
+++ admin/includes/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+      <div class="container-fluid py-1 px-3">
+
+
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </nav>
