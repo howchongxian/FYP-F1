32d527361089578764aef74185588dcf68b7b4c0
howchongxian <1211210949@student.mmu.edu.my>
2024-05-06 07:33:02 +0800

    update latest

--- videos.php
+++ videos.php
@@ -47,6 +47,12 @@
   <h1>F1 Videos</h1>
   <div class="video-container">
     <div class="video-card">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/fPJR0KU4e0g?si=irm1hlCynxFI5Jbu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h3>Race Highlights | Formula 1 Miami Grand Prix 2024</h3>
+      <a href="https://youtu.be/fPJR0KU4e0g?si=irm1hlCynxFI5Jbu" target="_blank">Watch on YouTube</a>
+    </div>
+
+    <div class="video-card">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/05hJ3GZME58?si=ZtN5hhkFHSTyzcii" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <h3>Sprint Race Highlights | Formula 1 Miami Grand Prix 2024</h3>
       <a href="https://youtu.be/05hJ3GZME58?si=ZtN5hhkFHSTyzcii" target="_blank">Watch on YouTube</a>
