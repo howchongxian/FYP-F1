1b5660142b7d10163ea6e19c11d714413127afd7
ShawnKohEnWee <1211211185@student.mmu.edu.my>
2024-05-26 23:07:06 +0800

    change admin order page

--- admin_order_detail.php
+++ admin_order_detail.php
@@ -41,7 +41,7 @@ include 'sidebar.php';
 ?>
 
 <body>
-    <div id="product_shop">
+    <div id="admin_product">
         <h1>F1 Product Shop - Orders</h1>
         <div class="product-list">
             <h2>Orders</h2>
