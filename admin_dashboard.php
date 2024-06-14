3f34054d18c5706aef6e12e23650e198f5e67678
1211110578 <1211110578@student.mmu.edu.my>
2024-06-14 21:19:02 +0800

    .

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -10,7 +10,7 @@ include 'dataconnection.php';
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Dashboard</title>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-   <link rel="stylesheet" href="css/admin_dashboard.css">
+   <link rel="stylesheet" href="./css/admin_dashboard.css">
 </head>
 <body>
 <?php include 'admin_sidebar.php'; ?>
