4281136bf358b7273cc23127d64e6381ac168925
howchongxian <1211210949@student.mmu.edu.my>
2024-06-23 17:24:09 +0800

    update

--- videos.php
+++ videos.php
@@ -58,9 +58,15 @@
   <h1>F1 Videos</h1>
   <div class="video-container">
   <div class="video-card">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/eSY_kdO4oEY?si=lxqTbP0qIjIj_NvW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <h3>Race Highlights | Formula 1 Canadian Grand Prix 2024</h3>
-      <a href="https://youtu.be/eSY_kdO4oEY?si=lxqTbP0qIjIj_NvW" target="_blank">Watch on YouTube</a>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/nWTXV9CHY2k?si=hAN7Ai2efatwQerC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <h3>Qualifying Highlights | Formula 1 Spanish Grand Prix 2024</h3>
+    <a href="https://youtu.be/nWTXV9CHY2k?si=hAN7Ai2efatwQerC" target="_blank">Watch on YouTube</a>
+  </div>
+
+  <div class="video-card">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/eSY_kdO4oEY?si=lxqTbP0qIjIj_NvW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <h3>Race Highlights | Formula 1 Canadian Grand Prix 2024</h3>
+    <a href="https://youtu.be/eSY_kdO4oEY?si=lxqTbP0qIjIj_NvW" target="_blank">Watch on YouTube</a>
   </div>
   
   <div class="video-card">
