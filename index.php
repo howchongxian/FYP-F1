af6d76a48c93a5e8eeef6359d517261a01b494b8
howchongxian <1211210949@student.mmu.edu.my>
2024-07-01 09:06:00 +0800

    latest

--- index.php
+++ index.php
@@ -89,7 +89,6 @@ $(document).ready(function () {
       <a href="https://www.formula1.com/en/latest/article/i-feel-let-down-norris-says-verstappen-in-the-wrong-as-both-drivers-give.3YMA6QdZk5Ih4dKRK2nelR" target="_blank">Read More</a>
   </div>
 
-  <div class="news-container">
   <div class="news-card">
     <img src="images/sainz.jpg" alt="F1 News 2" width="100%">
     <h3>Sainz happy with ‘bonus’ podium in Austria while Leclerc reflects on first-lap clash with Piastri and Perez</h3>
@@ -97,7 +96,6 @@ $(document).ready(function () {
     <a href="https://www.formula1.com/en/latest/article/sainz-happy-with-bonus-podium-in-austria-while-leclerc-reflects-on-first-lap.5ZTTeOIGnAgKLnfsx6bziR" target="_blank">Read More</a>
   </div>
 
-<div class="news-container">
   <div class="news-card">
     <img src="images/Power_Rankings_2024.jpg" alt="F1 News 3" width="100%">
     <h3>POWER RANKINGS: Which drivers stood out and impressed our judges at the Japanese Grand Prix weekend?</h3>
@@ -111,6 +109,7 @@ $(document).ready(function () {
     <p>Sergio Perez expects his Red Bull and F1 future to be clarified within the next month, with the Mexican out of contract next year.</p>
     <a href="https://www.formula1.com/en/latest/article/perez-expects-decision-on-red-bull-future-to-be-made-within-next-month.4f3CISx1VFIFMzF4qnm5Yy" target="_blank">Read More</a>
   </div>
+</div>
 
 <a href="news.php">
   <button id="more-news-button">More News</button>
@@ -118,24 +117,23 @@ $(document).ready(function () {
 
 <div class="video-container">
   <div class="video-card">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/7EP9p8NIJx8?si=guC8lEb6yC9-64E8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <h3>Race Highlights | Formula 1 Japanese Grand Prix 2024</h3>
-      <a href="https://youtu.be/7EP9p8NIJx8?si=f3h_7lXLvwzENxs5" target="_blank">Watch on YouTube</a>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/2T32QkF8MCg?si=z79d1DsvQHgupYLT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <h3>Race Highlights | Formula 1 Austrian Grand Prix 2024</h3>
+    <a href="https://youtu.be/2T32QkF8MCg?si=z79d1DsvQHgupYLT" target="_blank">Watch on YouTube</a>
   </div>
 
   <div class="video-card">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/T3ulO7DjKjc?si=ewhtgBKuhOX-ssn6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <h3>Race Highlights | Formula 1 Australian Grand Prix 2024</h3>
-      <a href="https://youtu.be/T3ulO7DjKjc?si=zlLM5VtY0hXCzIHR" target="_blank">Watch on YouTube</a>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/hYBIhCJ0vzU?si=EZd10-5CQRx8rXmM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <h3>Race Highlights | Formula 1 Spanish Grand Prix 2024</h3>
+    <a href="https://youtu.be/hYBIhCJ0vzU?si=EZd10-5CQRx8rXmM" target="_blank">Watch on YouTube</a>
   </div>
 
   <div class="video-card">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/VAxQq4RLvTg?si=HdSCXsVlPejGY4vD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <h3>Race Highlights | Formula 1 Saudi Arabian Grand Prix 2024</h3>
-      <a href="https://youtu.be/VAxQq4RLvTg?si=rvf1cyzlK7aOEw3u" target="_blank">Watch on YouTube</a>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/eSY_kdO4oEY?si=lxqTbP0qIjIj_NvW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <h3>Race Highlights | Formula 1 Canadian Grand Prix 2024</h3>
+    <a href="https://youtu.be/eSY_kdO4oEY?si=lxqTbP0qIjIj_NvW" target="_blank">Watch on YouTube</a>
   </div>
 </div>
-
 <a href="videos.php">
   <button id="more-videos-button">More Videos</button>
 </a>
@@ -144,10 +142,10 @@ $(document).ready(function () {
   <section>
       <h2>Upcoming Races</h2>
       <ul>
-          <li><span class="grand-prix">FORMULA 1 LENOVO CHINESE GRAND PRIX 2024</span><br><span class="date">19-21 April 2024</span></li>
-          <li><span class="grand-prix">FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2024</span><br><span class="date">4-6 May 2024</span></li>
-          <li><span class="grand-prix">FORMULA 1 MSC CRUISES GRAN PREMIO DEL MADE IN ITALY E DELL'EMILIA-ROMAGNA 2024</span><br><span class="date">17-19 May 2024</span></li>
-          <li><span class="grand-prix">FORMULA 1 GRAND PRIX DE MONACO 2024</span><br><span class="date">24-26 May 2024</span></li>
+          <li><span class="grand-prix">FORMULA 1 QATAR AIRWAYS BRITISH GRAND PRIX 2024</span><br><span class="date">5-7 July 2024</span></li>
+          <li><span class="grand-prix">FORMULA 1 HUNGARIAN GRAND PRIX 2024</span><br><span class="date">19-21 July 2024</span></li>
+          <li><span class="grand-prix">FORMULA 1 ROLEX BELGIAN GRAND PRIX 2024</span><br><span class="date">26-28 July 2024</span></li>
+          <li><span class="grand-prix">FORMULA 1 HEINEKEN DUTCH GRAND PRIX 2024</span><br><span class="date">23-25 August 2024</span></li>
       </ul>
   </section>
 </a>
